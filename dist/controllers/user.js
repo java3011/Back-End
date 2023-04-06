3daf84ed6be3e344af52b9a40a144f84d191d3e8
java3011 <72117272+java3011@users.noreply.github.com>
2023-04-06 18:10:10 -0400

    Add files via upload

---
+++ dist/controllers/user.js
@@ -0,0 +1,73 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loginUser = exports.newUser = void 0;
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const user_1 = require("../models/user");
+const newUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, email, charge, password } = req.body;
+    //Validar existencia del usuario en BD
+    const user = yield user_1.User.findOne({ where: { username: username } });
+    if (user) {
+        return res.status(400).json({
+            msg: `Ya existe un usuario con el nombre ${username}`
+        });
+    }
+    const hashedPassword = yield bcrypt_1.default.hash(password, 10);
+    try {
+        //Guardar usuarios en la BD
+        yield user_1.User.create({
+            username: username,
+            email: email,
+            charge: charge,
+            password: hashedPassword
+        });
+        res.json({
+            msg: `Usuario ${username} creado exitosamente!`
+        });
+    }
+    catch (error) {
+        res.status(400).json({
+            msg: 'Ups, ocurrio un error',
+            error
+        });
+    }
+});
+exports.newUser = newUser;
+const loginUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    //Validar si el usuario existe en la base de datos
+    const user = yield user_1.User.findOne({ where: { email: email } });
+    if (!user) {
+        return res.status(400).json({
+            msg: `No existe un usuario con el nombre ${email} en la base de datos`
+        });
+    }
+    //Validar el password
+    const passwordValid = yield bcrypt_1.default.compare(password, user.password);
+    if (!passwordValid) {
+        return res.status(400).json({
+            msg: `Password Incorrecta`
+        });
+    }
+    //Validar token
+    const token = jsonwebtoken_1.default.sign({
+        email: email
+    }, process.env.SECRET_KEY || 'pepito123', {
+        expiresIn: '10000'
+    });
+    res.json(token);
+});
+exports.loginUser = loginUser;
